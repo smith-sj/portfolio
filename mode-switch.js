7ddad4f4d968102c55d76929b6fc6fcae5238b96
smith-sj <mr.sjsmith@protonmail.com>
2022-03-11 22:09:04 +1100

    started complete refactor of HTML and CSS

--- mode-switch.js
+++ mode-switch.js
@@ -1,16 +1,15 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector('.theme-toggle input[type="checkbox"]');
 
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark'); //add this
+        localStorage.setItem('theme', 'dark');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light'); //add this
+        localStorage.setItem('theme', 'light');
     }    
 }
-
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
